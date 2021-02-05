bedb044050f7a4122d0ede0d28f207eac45b0e21
Franco <franco.bianco93@gmail.com>
2021-02-05 01:56:19 -0300

    Setup app for Heroku

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Setup handlebars and view location
 app.set('view engine', 'hbs');
@@ -77,6 +78,6 @@ app.get('*', (req, res) => {
     });
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
